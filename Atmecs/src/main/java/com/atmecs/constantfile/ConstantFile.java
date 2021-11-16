aa4406f8bb7096f6c3c67f8becc296ead358ac87
Ravikumar Kannan <ravikumar.kannan@atmecs.com>
2021-11-16 12:26:33 +0530

    added file in constants.java

--- Atmecs/src/main/java/com/atmecs/constantfile/ConstantFile.java
+++ Atmecs/src/main/java/com/atmecs/constantfile/ConstantFile.java
@@ -19,4 +19,6 @@ public class ConstantFile {
 	
 	public final static String validation_Home = file_Home + "atmecs.resources" + File.separator + "validationdata";
 	public final static String validation_File= validation_Home;
+	
+	public final static String test = "Ravi kumar";
 } 
